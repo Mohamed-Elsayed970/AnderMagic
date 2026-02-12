76c5a0f5a7a18bb41ed0a711fe185e75c0b3ac3d
El7azzo <hozyfabatisha@gmail.com>
2026-02-12 06:59:01 +0200

    remove gui from home screen

--- scirap/gamemanger.gd
+++ scirap/gamemanger.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
-var  cions = 0
+var cions = 0
 var scor = 0
 
-func _process(delta: float) -> void:
+func _start() -> void:
+	$gui.visible = true
 	
-	$"gui/coins_score".text=str(cions)
-	$"gui/myscore".text=str(scor)
+func _ready() -> void:
+	$gui.visible = false
+
+func _process(delta: float) -> void:
+	$"gui/coins_score".text = str(cions)
+	$"gui/myscore".text = str(scor)
+	pass
--- scirap/ui.gd
+++ scirap/ui.gd
@@ -1,19 +1,14 @@
 extends Control
 
 
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://sanse/game.tscn")
+	GameM._start()
 	pass 
-
-
-
-
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	
-
 
 func _on_settings_12_pressed() -> void:
 	get_tree().change_scene_to_file("res://sanse/settings.tscn")
