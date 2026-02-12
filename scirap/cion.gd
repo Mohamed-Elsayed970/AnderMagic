1bd47782320b9dc0ff4dde954b4ffd5a3c313853
Eng.Mohammed Elsayed <mohammedalseyed9007@gmail.com>
2026-02-12 04:42:22 +0200

    Update UI themes and game manager logic; add ascent and descent properties to theme files, modify main scene reference, and clean up unused nodes in UI.

--- scirap/cion.gd
+++ scirap/cion.gd
@@ -2,9 +2,6 @@ extends AnimatedSprite2D
 
 
 
-
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GameM.cions+=1
--- scirap/gamemanger.gd
+++ scirap/gamemanger.gd
@@ -4,5 +4,6 @@ var  cions = 0
 var scor = 0
 
 func _process(delta: float) -> void:
+	
 	$"gui/coins_score".text=str(cions)
 	$"gui/myscore".text=str(scor)
