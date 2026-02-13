c7ae1c0eb08868c8eb22ad7bd2e60a11df24c561
Eng.Mohammed Elsayed <mohammedalseyed9007@gmail.com>
2026-02-13 05:16:49 +0200

    Implement audio bus layout and enhance settings UI with music and SFX controls

---
+++ scirap/control.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+
+
+
+
+func _on_baek_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://sanse/settings.tscn")
--- scirap/settings.gd
+++ scirap/settings.gd
@@ -7,3 +7,10 @@ extends Control
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://sanse/ui.tscn")
+
+
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://sanse/control.tscn")
